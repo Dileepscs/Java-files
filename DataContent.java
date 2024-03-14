eada44be120c78d3be7619a5e415c9f427175248
Dileepscs <101684663+Dileepscs@users.noreply.github.com>
2024-03-14 09:38:00 +0530

    Add files via upload

---
+++ DataContent.java
@@ -0,0 +1,13 @@
+package task2_JDBC_AWT;
+
+public class DataContent {
+
+	public String[] getColumns() {
+		return null;
+	}
+
+	public String[][] getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
